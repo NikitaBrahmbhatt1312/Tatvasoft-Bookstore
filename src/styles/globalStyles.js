c2d3df768c760f2aca3073171a0ee4b98c4f3359
NikitaBrahmbhatt1312 <125294915+NikitaBrahmbhatt1312@users.noreply.github.com>
2023-08-16 10:52:31 -0500

    Add files via upload

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,1 +1,28 @@
-
+export const globalStyles = {
+    navbar: {
+        padding: 20,
+        display: "flex",
+        justifyContent: "space-around",
+        backgroundColor: "#FFB200",
+        color: "white",
+    },
+    navLink: {
+        paddingLeft: 20,
+    },
+    formDemo: {
+        display: "flex",
+        flexDirection: "column",
+        width: "400px",
+        margin: "auto",
+        marginTop: 30,
+        gap: 20,
+    },
+    // main: {
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    // },
+    // imgWrapper: {
+    //     width: 300,
+    // },
+};
